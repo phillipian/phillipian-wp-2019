d57909fbdbdb7ceec8cd81e3b60779a37877506d
wwsalmon <wwsamson@yahoo.com>
2019-02-21 08:38:31 -0500

    Fix Topbar Bug

--- header.php
+++ header.php
@@ -23,7 +23,7 @@
     <div class='navbar'>
         <div class='navbar-content'>
             <div class='navbar-logo'>
-                <a href='http://localhost/wordpress/'><img src='<?php echo wp_get_attachment_url(get_theme_mod(' plip-navbar-image')) ?>'></a>
+                <a href='http://localhost/wordpress/'><img src='<?php echo wp_get_attachment_url(get_theme_mod('plip-navbar-image')) ?>'></a>
             </div>
             <?php wp_nav_menu(array('theme_location' => 'primary')) ?>
             <!--class: menu-main-navigation-container-->
