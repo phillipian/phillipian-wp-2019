075a17df15d225baa8859865fcbc457de3abc0d5
wwsalmon <wwsamson@yahoo.com>
2019-02-23 15:14:37 -0500

    category.php initial work

--- category.php
+++ category.php
@@ -2,4 +2,23 @@
 
 <!-- do this stuff -->
 
-<?php get_footer(); ?>
+<div class='category-container'>
+    <h1><?php echo single_cat_title()?></h1>
+    <?php if (have_posts()): while (have_posts()): the_post(); ?>
+    <div class='article-item'>
+        <h2><a href='<?php the_permalink(); ?>'>
+                <?php the_title(); ?>
+            </a></h2>
+        <div class=' article-text'>
+            <div class='article-author'><span>By
+                    <?php the_author(); ?></span></div>
+            <?php the_excerpt(); ?>
+        </div>
+        <div class='article-image'><img src='<?php echo catch_that_image() ?>'></div>
+    </div>
+    <?php endwhile;
+    endif; ?>
+</div>
+
+
+<?php get_footer(); ?> 
