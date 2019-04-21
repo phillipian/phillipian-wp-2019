682977e689a4e37b71c9123096a485c5f1166526
wwsalmon <wwsamson@yahoo.com>
2019-04-21 16:07:48 -0400

    Temporarily disable author links

--- article-author-include.php
+++ article-author-include.php
@@ -5,14 +5,14 @@
     <script>console.log("<?php echo $def_author ?>");</script>
     <?php
     $cust_author = get_post_meta(get_the_ID(), 'cpa_author', true);
-    if ($def_author == 'admin') {
+    // if ($def_author == 'admin') {
         echo "<span>" . $cust_author . "</span>";
-    } else {
+    /*} else {
         if ($singlepage) {
             $author_email = get_the_author_meta('email');
             echo "<a href='" . get_author_posts_url(get_the_author_meta('ID')) . "'><div class='author-prof'>" . get_avatar($author_email) . "</div></a>";
         }
         echo "<span>" . get_the_author_posts_link() . "</span>";
-    }
+    }*/
     ?>
 </div> 
