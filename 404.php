57a4f59fef997ae7ea3f873eb1e35bf252741e65
wwsalmon <wwsamson@yahoo.com>
2019-04-22 00:24:48 -0400

    Fix sidebar links, add 404 page

---
+++ 404.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+
+<div class='category-container page-container'>
+            <h1>Page not found</h1>
+            <p>Whether you followed a bad link or an old link or Wordpress just hates you, you've come to a URL that supposedly doesn't exist. Check that your URL is correct and use the navbar to get where you need to be.</p>
+</div>
+
+
+<?php get_footer(); ?>
