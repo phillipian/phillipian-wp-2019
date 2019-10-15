765f74cdc484b2614bb87385821bdcb6d4a99886
wwsalmon <wwsamson@yahoo.com>
2019-10-14 22:55:17 -0400

    Fix Author Bug

--- article-author-include.php
+++ article-author-include.php
@@ -1,13 +1,26 @@
 <div class='article-author'>
-    <?php 
+    <?php
+    ?>
+    <?php
     $def_author = get_the_author();
-    if (function_exists('coauthors_posts_links')) {
-        // $coauthors = coauthors_posts_links();
-        coauthors();
-        // print_r($coauthors);
+    if ($def_author  == 'admin'){
+        $cust_author = get_post_meta(get_the_ID(), 'cpa_author', true);
+        echo $cust_author;
     }
     else{
-        the_author();
+        if (function_exists('coauthors_posts_links')) {
+            $coauthors = get_coauthors(get_the_ID());
+            $last = end($coauthors)->display_name;
+            foreach ($coauthors as $author) {
+                echo $author->display_name;
+                if (!($author->display_name == $last)) {
+                    echo ", ";
+                }
+            }
+        }
+        else{
+            echo $def_author->display_name . $def_author;
+        }
     }
     // if ($def_author == 'admin') {
         // echo "<span>" . $def_author . "</span>";
