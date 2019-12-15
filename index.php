115d701a6a19978fcc005ea3ebee5e9e7e49dba2
wwsalmon <wwsamson@yahoo.com>
2019-12-15 18:20:11 -0500

    commentary posts show up

--- index.php
+++ index.php
@@ -34,7 +34,7 @@
                 <p><?php echo get_theme_mod('plip-breaking-item2', null); ?></p>
                 <p><?php echo get_theme_mod('plip-breaking-item3', null); ?></p>
                 <?php query_posts(array(
-                        'category_name' => 'breaking'
+                        'category__and' => array(get_cat_ID("breaking"), get_cat_ID("news"))
                     )); ?>
 
                 <p>Follow <i>The Phillipian</i> on social media for the latest updates:</p>
@@ -86,12 +86,41 @@
                 <?php
                     endif; ?>
 
-                <div class='breaking-coverage-label'><span>Student Commentary</span></div>
+                <div class='breaking-coverage-label'><span>Community Commentary</span></div>
 
                 <p>Do you have thoughts about these policy changes? Start writing, be it a 100-word or 1000-word response, and email <a href='mailto:commentary@phillipian.net'>commentary@phillipian.net</a>.</p>
 
-                        <!-- <img src='<?php echo wp_get_attachment_url(get_theme_mod('plip-breaking-image', null)); ?>'> -->
-                        <!-- <div class='breaking-input'>
+                <?php query_posts(array(
+                        'category__and' => array(get_cat_ID("breaking"), get_cat_ID("commentary"))
+                    ));
+                    if (have_posts()) :
+                        while (have_posts()) :
+                            the_post(); ?>
+
+                        <a href='<?php the_permalink(); ?>'>
+                            <div class='breaking-article'>
+                                <div class='breaking-article-time'><?php
+                        $coauthors = get_coauthors(get_the_ID());
+                        $last = end($coauthors)->display_name;
+                        foreach ($coauthors as $author) {
+                            echo $author->display_name;
+                            if (!($author->display_name == $last)) {
+                                echo ", ";
+                            }
+                        }
+                                ?></div>
+                                <h2><?php the_title(); ?></h2>
+                            </div>
+                        </a>
+
+                    <?php endwhile;
+                        endif;
+                        ?>
+
+                
+
+                <!-- <img src='<?php echo wp_get_attachment_url(get_theme_mod('plip-breaking-image', null)); ?>'> -->
+                <!-- <div class='breaking-input'>
                     <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdeBchqSs3vzeXcJyZSZ8--B9TuKKyuktB_0UD4DK7Z1CQw_g/viewform?embedded=true" marginheight="0" marginwidth="0">Loading…</iframe>
                 </div> -->
             </div>
