216fde086447cf72872cf6c7b4c1877feec82f2e
wwsalmon <wwsamson@yahoo.com>
2019-12-26 15:40:34 -0500

    homepage responsiveness fixes

--- index.php
+++ index.php
@@ -25,40 +25,46 @@
         <div class="breaking-banner">
             <span>Breaking News</span>
         </div>
-        <div class="breaking-story three-col">
-            <div class="breaking-info">
-                <h1>Uppers Will No Longer Be Allowed to Attend Prom</h1>
-                <p>Seniors received an email from Jennifer Elliott '94, Dean of Students and Residential Life,
-                    announcing that Commencement weekend will be "shift[ing] entirely to celebrating our Seniors'
-                    Commencement Weekend." Elliott stated that all underclassmen will depart on June 4th, and Prom will
-                    be held on June 5th.</p>
+        <div class="breaking-story-outer">
+            <div class="breaking-story three-col">
+                <div class="breaking-info">
+                    <h1>Uppers Will No Longer Be Allowed to Attend Prom</h1>
+                    <p>Seniors received an email from Jennifer Elliott '94, Dean of Students and Residential Life,
+                        announcing that Commencement weekend will be "shift[ing] entirely to celebrating our Seniors'
+                        Commencement Weekend." Elliott stated that all underclassmen will depart on June 4th, and Prom will
+                        be held on June 5th.</p>
+                </div>
+                <div class="breaking-col">
+                    <div class="breaking-coverage-label"><span>Our Latest Coverage</span></div>
+                    <?php query_posts(array(
+                        'category__and' => array(get_cat_ID("breaking"), get_cat_ID("news"))
+                    ));
+                    if (have_posts()) :
+                        while (have_posts()) :
+                            the_post();
+                            include "article-include.php";
+                        endwhile;
+                    endif; ?>
+                </div>
+                <div class="breaking-col">
+                    <div class="breaking-coverage-label"><span>Community Commentary</span></div>
+                </div>
             </div>
-            <div class="breaking-col">
-                <div class="breaking-coverage-label"><span>Our Latest Coverage</span></div>
-                <?php query_posts(array(
-                    'category__and' => array(get_cat_ID("breaking"), get_cat_ID("news"))
-                ));
-                if (have_posts()) :
-                    while (have_posts()) :
-                        the_post();
-                        include "article-include.php";
-                    endwhile;
-                endif; ?>
-            </div>
-            <div class="breaking-col">
-                <div class="breaking-coverage-label"><span>Community Commentary</span></div>
-            </div>
+            <div class="breaking-scroll"><span>Scroll right for more <i class="fas fa-arrow-right"></i></span></div>
         </div>
-        <div class="breaking-story three-col">
-            <div class="breaking-info">
-                <h1>Dr. Raynard S. Kington Announced 16th Head of School</h1>
+        <div class="breaking-story-outer">
+            <div class="breaking-story three-col">
+                <div class="breaking-info">
+                    <h1>Dr. Raynard S. Kington Announced 16th Head of School</h1>
+                </div>
+                <div class="breaking-col">
+                    <div class="breaking-coverage-label"><span>Our Latest Coverage</span></div>
+                </div>
+                <div class="breaking-col">
+                    <div class="breaking-coverage-label"><span>Videos</span></div>
+                </div>
             </div>
-            <div class="breaking-col">
-                <div class="breaking-coverage-label"><span>Our Latest Coverage</span></div>
-            </div>
-            <div class="breaking-col">
-                <div class="breaking-coverage-label"><span>Videos</span></div>
-            </div>
+            <div class="breaking-scroll"><span>Scroll right for more <i class="fas fa-arrow-right"></i></span></div>
         </div>
     </div>
     <div class="home-featured four-col">
