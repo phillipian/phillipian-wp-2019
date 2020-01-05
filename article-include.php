4a474c09aeaaab0da3bc74deb463b4a7bb522228
wwsalmon <wwsamson@yahoo.com>
2020-01-05 00:46:52 -0500

    Basic Multimedia Support

--- article-include.php
+++ article-include.php
@@ -1,13 +1,36 @@
+<?php
+if (has_category("multimedia")):  // FOR MULTIMEDIA ARTICLE  ?>
+
+
+<div class="article-item <?php if ($archive) { ?>article-archive <?php } elseif ($sect) { ?>article-sect <?php } else { ?>article-news-side <?php } ?> article-noimage article-multimedia ">
+    <div class="article-text">
+
+        <div class='article-category'><?php catsNoFeatured(); ?></div>
+        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+        <?php
+
+        $postcontent = get_the_content();
+        preg_match("/\[ytembed(.*)\[\/ytembed\]/", $postcontent, $youtubematches);
+        echo do_shortcode($youtubematches[0]);
+
+        ?>
+        <?php include 'article-author-include.php' ?>
+        <div class="article-date"><span><?php the_time("M j"); ?></span></div>
+    </div>
+</div>
+
+<?php else: // NOT MULTIMEDIA ARTICLE ?>
+
 <div class="article-item <?php if ($archive) { ?>article-archive <?php } elseif ($sect) { ?>article-sect <?php } else { ?>article-news-side <?php }
 if (catch_that_image() == false) : ?>article-noimage<?php endif ?>">
     <div class=" article-text">
-        <div class='article-category'><a href=''><span><?php if ($catname == "Sports") {
+        <div class='article-category'><?php if ($catname == "Sports") {
                         catsSports();
                     } elseif ($multilingual) {
                         catMulti();
                     } else {
                         catsNoFeatured();
-                    } ?></span></a></div>
+                    } ?></div>
         <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
         <?php include 'article-author-include.php' ?>
         <div class="article-date"><span><?php the_time("M j"); ?></span></div>
@@ -18,3 +41,4 @@ if (catch_that_image() == false) : ?>article-noimage<?php endif ?>">
     <?php endif ?>
     <?php the_scorebox(); ?>
 </div>
+<?php endif; ?>
