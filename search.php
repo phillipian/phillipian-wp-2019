5724f097f896f1a2e93259346e4e93a298cbc316
wwsalmon <wwsamson@yahoo.com>
2020-01-06 22:29:47 -0500

    Fix search page with new format

--- search.php
+++ search.php
@@ -16,13 +16,31 @@ $the_query = new WP_Query($search_query);
     <h1>
         Search results for "<?php echo the_search_query() ?>"
     </h1>
-    <?php if ($the_query->have_posts()) : while ($the_query->have_posts()) : $the_query->the_post();
-    $archive = true;
-    include 'includes/include-article-item.php';
-    endwhile;
-else : ?>
-    <p>No results found.</p>
+
+
+    <?php if ($the_query->have_posts()) : ?>
+        <div class="category-posts-container">
+            <?php while ($the_query->have_posts()) : $the_query->the_post();
+                $archive = true;
+                include 'includes/include-article-item.php';
+            endwhile; ?>
+        </div>
+
+        <script>
+            var $grid = $(".category-posts-container").masonry({
+                itemSelector: '.article-item',
+                percentPosition: true,
+                gutter: 36,
+                horizontalOrder: true,
+                columnWidth: '.article-item'
+            });
+            $grid.imagesLoaded(function () {
+                $grid.masonry();
+            });
+        </script>
+    <?php else : ?>
+        <p>No results found.</p>
     <?php
-endif; ?>
+    endif; ?>
 </div>
 <?php get_footer(); ?> 
