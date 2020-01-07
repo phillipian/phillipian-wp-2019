258dde51ea8a981d3b6bd61de66144e6131e174b
wwsalmon <wwsamson@yahoo.com>
2020-01-07 17:22:43 -0500

    Fix #27, better 404 message

--- 404.php
+++ 404.php
@@ -2,7 +2,7 @@
 
 <div class='category-container page-container'>
             <h1>Page not found</h1>
-            <p>Whether you followed a bad link or an old link or Wordpress just hates you, you've come to a URL that supposedly doesn't exist. Check that your URL is correct and use the navbar to get where you need to be.</p>
+            <p>Whether you followed a bad link or due to a technical error, you've come to a URL that supposedly doesn't exist. Check that your URL is correct and use the navbar to get where you need to be.</p>
 </div>
 
 
