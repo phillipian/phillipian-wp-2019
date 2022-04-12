39068b4d522a62c95f5ecd758708b3680e1856e6
Claire Wang <41759033+ClaireBookworm@users.noreply.github.com>
2022-04-12 00:33:42 -0400

    Update page-subscribe.php

--- page-subscribe.php
+++ page-subscribe.php
@@ -5,7 +5,7 @@
         <div class='subscribe-text'>
             <p class='subscribe-hero'>Subscribe to support <?php echo (date("Y") - 1878)?> years of uncensored student journalism and have the latest news about Andover delivered to your doorstep.</p>
             <p>In addition to being published online and distributed around campus, <i>The Phillipian</i> offers off-campus subscriptions to have the printed paper delivered to you each week.</p>
-            <p>For the 2018-19 school year (29 issues) we offer the following plans:</p>
+            <p>For the 2021-2022 school year (29 issues) we offer the following plans:</p>
             <ul>
                 <li>Off-campus (Domestic): $185</li>
                 <li>Off-campus (International): $225</li>
